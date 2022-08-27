a940236f6ecd8608fe5911c45d2d1ddfb350f699
Elisha Ukpong <ishukpong418@gmail.com>
2022-08-27 23:35:59 +0100

    addd top buyers lens

---
+++ app/Nova/Lenses/TopBuyers.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Nova\Lenses;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\LensRequest;
+use Laravel\Nova\Lenses\Lens;
+
+class TopBuyers extends Lens
+{
+    /**
+     * Get the query builder / paginator for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\LensRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return mixed
+     */
+    public static function query(LensRequest $request, $query)
+    {
+        return $request->withOrdering($request->withFilters(
+            $query
+                ->select(DB::Raw('users.id as `id`, users.name as `name`, sum(product.price) as `total'))
+                ->join('orders', 'users.id', '=', 'orders.user_id')
+                ->join('products', 'products.id', '=', 'orders.product_id')
+                ->groupBy('users.id')
+        ));
+    }
+
+    /**
+     * Get the fields available to the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')
+                ->sortable(),
+            Text::make('Name'),
+            Text::make('Total'),
+        ];
+    }
+
+    /**
+     * Get the cards available on the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available on the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return parent::actions($request);
+    }
+
+    /**
+     * Get the URI key for the lens.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'top-buyers';
+    }
+}
